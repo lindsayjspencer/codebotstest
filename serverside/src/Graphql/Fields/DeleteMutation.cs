0000f83190ff10fd0fc06b54658fcf46dbf8efde
Dev User <dev@codebots.com>
2020-11-05 23:09:16 +0000

    Re-write target application

---
+++ serverside/src/Graphql/Fields/DeleteMutation.cs
@@ -0,0 +1,100 @@
+/*
+ * @bot-written
+ * 
+ * WARNING AND NOTICE
+ * Any access, download, storage, and/or use of this source code is subject to the terms and conditions of the
+ * Full Software Licence as accepted by you before being granted access to this source code and other materials,
+ * the terms of which can be accessed on the Codebots website at https://codebots.com/full-software-licence. Any
+ * commercial use in contravention of the terms of the Full Software Licence may be pursued by Codebots through
+ * licence termination and further legal action, and be required to indemnify Codebots for any loss or damage,
+ * including interest and costs. You are deemed to have accepted the terms of the Full Software Licence on any
+ * access, download, storage, and/or use of this source code.
+ * 
+ * BOT WARNING
+ * This file is bot-written.
+ * Any changes out side of "protected regions" will be lost next time the bot makes any changes.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using My2000thapp.Graphql.Helpers;
+using My2000thapp.Graphql.Types;
+using My2000thapp.Helpers;
+using My2000thapp.Models;
+using My2000thapp.Services;
+using GraphQL;
+using GraphQL.Types;
+
+namespace My2000thapp.Graphql.Fields
+{
+	public class DeleteMutation
+	{
+		/// <summary>
+		/// Creates a mutation that will delete things from the database
+		/// </summary>
+		/// <param name="name">The name of the model to delete</param>
+		/// <typeparam name="TModel">The type of the model to delete</typeparam>
+		/// <returns>A function that takes a graphql context and returns a list the deleted ids</returns>
+		public static Func<ResolveFieldContext<object>, Task<object>> CreateDeleteMutation<TModel>(string name)
+			where TModel : class, IOwnerAbstractModel, new()
+		{
+			return async context =>
+			{
+				var graphQlContext = (My2000thappGraphQlContext) context.UserContext;
+				var crudService = graphQlContext.CrudService;
+				var ids = context.GetArgument<List<Guid>>($"{name}Ids".ToCamelCase());
+
+				try
+				{
+					if (ids == null)
+					{
+						throw new AggregateException(new Exception("No ids provided to delete, aborting!"));
+					}
+
+					var deletedIds = await crudService.Delete<TModel>(ids);
+					return IdObject.FromList(deletedIds);
+				}
+				catch (AggregateException exception)
+				{
+					context.Errors.AddRange(
+						exception.InnerExceptions.Select(error => new ExecutionError(error.Message)));
+					return new List<TModel>();
+				}
+			};
+		}
+
+		/// <summary>
+		/// Creates a mutation that will delete things from the database by a where condition
+		/// </summary>
+		/// <param name="name">The name of the model to delete</param>
+		/// <typeparam name="TModel">The type of the model to delete</typeparam>
+		/// <returns>A function that takes a graphql context and returns whether the delete is successful</returns>
+		public static Func<ResolveFieldContext<object>, Task<object>> CreateConditionalDeleteMutation<TModel>(string name)
+			where TModel : class, IOwnerAbstractModel, new()
+		{
+			return async context =>
+			{
+				var graphQlContext = (My2000thappGraphQlContext)context.UserContext;
+				var crudService = graphQlContext.CrudService;
+				var user = graphQlContext.User;
+				var dbSet = graphQlContext.DbContext.Set<TModel>().AsQueryable();
+
+				var models = QueryHelpers.CreateConditionalWhere(context, dbSet);
+				models = QueryHelpers.CreateIdsCondition(context, models);
+				models = QueryHelpers.CreateIdCondition(context, models);
+
+				try
+				{
+					return await crudService.ConditionalDelete(models);
+				}
+				catch (AggregateException exception)
+				{
+					context.Errors.AddRange(
+						exception.InnerExceptions.Select(error => new ExecutionError(error.Message)));
+					return false;
+				}
+			};
+		}
+	}
+}
---
+++ serverside/src/Graphql/Schema.cs
@@ -0,0 +1,255 @@
+/*
+ * @bot-written
+ * 
+ * WARNING AND NOTICE
+ * Any access, download, storage, and/or use of this source code is subject to the terms and conditions of the
+ * Full Software Licence as accepted by you before being granted access to this source code and other materials,
+ * the terms of which can be accessed on the Codebots website at https://codebots.com/full-software-licence. Any
+ * commercial use in contravention of the terms of the Full Software Licence may be pursued by Codebots through
+ * licence termination and further legal action, and be required to indemnify Codebots for any loss or damage,
+ * including interest and costs. You are deemed to have accepted the terms of the Full Software Licence on any
+ * access, download, storage, and/or use of this source code.
+ * 
+ * BOT WARNING
+ * This file is bot-written.
+ * Any changes out side of "protected regions" will be lost next time the bot makes any changes.
+ */
+using System;
+using System.Threading.Tasks;
+using My2000thapp.Graphql.Fields;
+using My2000thapp.Graphql.Helpers;
+using My2000thapp.Graphql.Types;
+using My2000thapp.Models;
+using My2000thapp.Models.RegistrationModels;
+using GraphQL;
+using GraphQL.EntityFramework;
+using GraphQL.Types;
+// % protected region % [Add any further imports here] off begin
+// % protected region % [Add any further imports here] end
+
+namespace My2000thapp.Graphql
+{
+	/// <summary>
+	/// The GraphQL schema class for fetching and mutating data
+	/// </summary>
+	public class My2000thappSchema : Schema
+	{
+		public My2000thappSchema(IDependencyResolver resolver) : base(resolver)
+		{
+			Query = resolver.Resolve<My2000thappQuery>();
+			Mutation = resolver.Resolve<My2000thappMutation>();
+			// % protected region % [Add any extra schema constructor options here] off begin
+			// % protected region % [Add any extra schema constructor options here] end
+		}
+
+		// % protected region % [Add any schema methods here] off begin
+		// % protected region % [Add any schema methods here] end
+	}
+
+	/// <summary>
+	/// The query class for the GraphQL schema
+	/// </summary>
+	public class My2000thappQuery : QueryGraphType<My2000thappDBContext>
+	{
+		private const string WhereDesc = "A list of where conditions that are joined with an AND";
+		private const string ConditionalWhereDesc = "A list of lists of where conditions. The conditions inside the " +
+													"innermost lists are joined with and OR and the results of those " +
+													"lists are joined with an AND";
+
+		public My2000thappQuery(IEfGraphQLService<My2000thappDBContext> efGraphQlService) : base(efGraphQlService)
+		{
+			// Add query types for each entity
+
+			// Add query types for each many to many reference
+
+			// % protected region % [Add any extra query config here] off begin
+			// % protected region % [Add any extra query config here] end
+		}
+
+		/// <summary>
+		/// Adds single, multiple and connection queries to query
+		/// </summary>
+		/// <typeparam name="TModelType">The GraphQL type for returning data</typeparam>
+		/// <typeparam name="TModel">The EF model type for querying the DB</typeparam>
+		/// <param name="name">The name of the entity</param>
+		public void AddModelQueryField<TModelType, TModel>(string name)
+			where TModelType : ObjectGraphType<TModel>
+			where TModel : class, IOwnerAbstractModel, new()
+		{
+			AddQueryField(
+				$"{name}s",
+				QueryHelpers.CreateResolveFunction<TModel>(),
+				typeof(TModelType)).Description = $"Query for fetching multiple {name}s";
+
+			AddSingleField(
+				name: name,
+				resolve: QueryHelpers.CreateResolveFunction<TModel>(),
+				graphType: typeof(TModelType)).Description = $"Query for fetching a single {name}";
+
+			AddQueryConnectionField(
+				$"{name}sConnection",
+				QueryHelpers.CreateResolveFunction<TModel>(),
+				typeof(TModelType));
+
+			FieldAsync<NumberObjectType>(
+				$"count{name}s",
+				arguments: new QueryArguments(
+					new QueryArgument<IdGraphType> { Name = "id" },
+					new QueryArgument<ListGraphType<IdGraphType>> { Name = "ids" },
+					new QueryArgument<ListGraphType<WhereExpressionGraph>>
+					{
+						Name = "where",
+						Description = WhereDesc
+					}
+				),
+				resolve: CountQuery.CreateCountQuery<TModel>(),
+				description: "Counts the number of models according to a given set of conditions"
+			);
+
+			AddQueryField(
+				$"{name}sConditional",
+				ConditionalQuery.CreateConditionalQuery<TModel>(),
+				typeof(TModelType),
+				new QueryArguments(
+					new QueryArgument<ListGraphType<ListGraphType<WhereExpressionGraph>>>
+					{
+						Name = "conditions",
+						Description = ConditionalWhereDesc
+					}
+				)
+			);
+
+			FieldAsync<NumberObjectType>(
+				$"count{name}sConditional",
+				arguments: new QueryArguments(
+					new QueryArgument<IdGraphType> { Name = "id" },
+					new QueryArgument<ListGraphType<IdGraphType>> { Name = "ids" },
+					new QueryArgument<ListGraphType<ListGraphType<WhereExpressionGraph>>>
+					{
+						Name = "conditions",
+						Description = ConditionalWhereDesc
+					}
+				),
+				resolve: CountQuery.CreateConditionalCountQuery<TModel>(),
+				description: "Counts the number of models according to a given set of conditions. This query can " +
+							"perform both AND and OR conditions"
+			);
+
+			// % protected region % [Add any extra per entity fields here] off begin
+			// % protected region % [Add any extra per entity fields here] end
+		}
+
+		// % protected region % [Add any extra query methods here] off begin
+		// % protected region % [Add any extra query methods here] end
+	}
+
+	/// <summary>
+	/// The mutation class for the GraphQL schema
+	/// </summary>
+	public class My2000thappMutation : ObjectGraphType<object>
+	{
+		private const string ConditionalWhereDesc = "A list of lists of where conditions. The conditions inside the " +
+											"innermost lists are joined with and OR and the results of those " +
+											"lists are joined with an AND";
+
+		public My2000thappMutation()
+		{
+			Name = "Mutation";
+
+			// Add input types for each entity
+
+			// Add input types for each many to many reference
+
+			// % protected region % [Add any extra mutation queries here] off begin
+			// % protected region % [Add any extra mutation queries here] end
+		}
+
+		/// <summary>
+		/// Adds the required mutation fields to the GraphQL schema for create, update and delete
+		/// </summary>
+		/// <typeparam name="TModelCreateInputType">The GraphQL input type used for the create functions</typeparam>
+		/// <typeparam name="TModelUpdateInputType">The GraphQL Input Type used for the update functions</typeparam>
+		/// <typeparam name="TModelType">The GraphQL model type used for returning data</typeparam>
+		/// <typeparam name="TModel">The EF model type for saving to the DB</typeparam>
+		/// <param name="name">The name of the entity</param>
+		/// <param name="createMutation">An override for the create mutation</param>
+		/// <param name="updateMutation">An override for the update mutation</param>
+		/// <param name="deleteMutation">An override for the delete mutation</param>
+		/// <param name="conditionalUpdateMutation">An override for the conditional update mutation</param>
+		/// <param name="conditionalDeleteMutation">An override for the conditional delete mutation</param>
+		public void AddMutationField<TModelCreateInputType, TModelUpdateInputType, TModelType, TModel>(
+			string name,
+			Func<ResolveFieldContext<object>, Task<object>> createMutation = null,
+			Func<ResolveFieldContext<object>, Task<object>> updateMutation = null,
+			Func<ResolveFieldContext<object>, Task<object>> deleteMutation = null,
+			Func<ResolveFieldContext<object>, Task<object>> conditionalUpdateMutation = null,
+			Func<ResolveFieldContext<object>, Task<object>> conditionalDeleteMutation = null)
+			where TModelCreateInputType : InputObjectGraphType<TModel>
+			where TModelUpdateInputType : InputObjectGraphType<TModel>
+			where TModelType : ObjectGraphType<TModel>
+			where TModel : class, IOwnerAbstractModel, new()
+		{
+			FieldAsync<ListGraphType<TModelType>>(
+				$"create{name}",
+				arguments: new QueryArguments(
+					new QueryArgument<ListGraphType<TModelCreateInputType>> { Name = name + "s" },
+					new QueryArgument<ListGraphType<StringGraphType>> { Name = "MergeReferences" }
+				),
+				resolve: createMutation ?? CreateMutation.CreateCreateMutation<TModel>(name)
+			);
+
+			FieldAsync<ListGraphType<TModelType>>(
+				$"update{name}",
+				arguments: new QueryArguments(
+					new QueryArgument<ListGraphType<TModelUpdateInputType>> { Name = name + "s" },
+					new QueryArgument<ListGraphType<StringGraphType>> { Name = "MergeReferences" }
+				),
+				resolve: updateMutation ?? UpdateMutation.CreateUpdateMutation<TModel>(name)
+			);
+
+			FieldAsync<ListGraphType<IdObjectType>>(
+				$"delete{name}",
+				arguments: new QueryArguments(
+					new QueryArgument<ListGraphType<IdGraphType>> { Name = $"{name}Ids" }
+				),
+				resolve: deleteMutation ?? DeleteMutation.CreateDeleteMutation<TModel>(name)
+			);
+
+			FieldAsync<BooleanObjectType>(
+				$"update{name}sConditional",
+				arguments: new QueryArguments(
+					new QueryArgument<IdGraphType> { Name = "id" },
+					new QueryArgument<ListGraphType<IdGraphType>> { Name = "ids" },
+					new QueryArgument<ListGraphType<ListGraphType<WhereExpressionGraph>>>
+					{
+						Name = "conditions",
+						Description = ConditionalWhereDesc
+					},
+					new QueryArgument<TModelUpdateInputType> { Name = "valuesToUpdate" },
+					new QueryArgument<ListGraphType<StringGraphType>> { Name = "fieldsToUpdate" }
+				),
+				resolve: conditionalUpdateMutation ?? UpdateMutation.CreateConditionalUpdateMutation<TModel>(name)
+			);
+
+			FieldAsync<BooleanObjectType>(
+				$"delete{name}sConditional",
+				arguments: new QueryArguments(
+					new QueryArgument<IdGraphType> { Name = "id" },
+					new QueryArgument<ListGraphType<IdGraphType>> { Name = "ids" },
+					new QueryArgument<ListGraphType<ListGraphType<WhereExpressionGraph>>>
+					{
+						Name = "conditions",
+						Description = ConditionalWhereDesc
+					}
+				),
+				resolve: conditionalDeleteMutation ?? DeleteMutation.CreateConditionalDeleteMutation<TModel>(name)
+			);
+
+			// % protected region % [Add any extra per entity mutations here] off begin
+			// % protected region % [Add any extra per entity mutations here] end
+		}
+
+		// % protected region % [Add any extra mutation methods here] off begin
+		// % protected region % [Add any extra mutation methods here] end
+	}
+}
